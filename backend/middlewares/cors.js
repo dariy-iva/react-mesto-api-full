f1eaaef841d3d1d8fb54f97286dc9f5765bc984e
Daria Ivanova <dashann@mail.ru>
2022-01-13 17:28:45 +0300

    fix/корректировка cors на сервере

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -9,12 +9,12 @@ module.exports = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  res.header('Access-Control-Allow-Credentials', true);
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
+  res.header('Access-Control-Allow-Credentials', true);
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
