548602ddf75b87bcbf39768485224f73a9084155
Daria Ivanova <dashann@mail.ru>
2022-01-13 17:54:39 +0300

    fix/cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -6,14 +6,14 @@ const allowedCors = [
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
+
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
   res.header('Access-Control-Allow-Credentials', true);
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
