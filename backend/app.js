29b12d6a48e54bd3e8d1222621622e26baf6acda
Daria Ivanova <dashann@mail.ru>
2022-01-14 21:11:52 +0300

    fix/cancel dotenv

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-require('dotenv').config();
+// require('dotenv').config();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const rateLimit = require('express-rate-limit');
